a9e2e552c98fbeae3dc692081ec722746fb1cc21
maurojcordoba <mauro.j.cordoba@gmail.com>
2022-02-11 18:53:15 -0300

    test

--- bgg.py
+++ bgg.py
@@ -7,16 +7,14 @@ def obtiene_coleccion_por_usuario(user) -> list:
     response = requests.get(url)
     data = xmltodict.parse(response.content)
     
-    if (data) :
-        print(data)
-        for item in data['items']['item']:
-            bgg_id = item['@objectid']
-            url_game =  f'https://boardgamegeek.com/boardgame/{bgg_id}'
-            game_list.append({
-                'name': item['name']['#text'],
-                'thumbnail': item['thumbnail'],
-                'owner': user,
-                'url_game': url_game
-                })
+    for item in data['items']['item']:
+        bgg_id = item['@objectid']
+        url_game =  f'https://boardgamegeek.com/boardgame/{bgg_id}'
+        game_list.append({
+            'name': item['name']['#text'],
+            'thumbnail': item['thumbnail'],
+            'owner': user,
+            'url_game': url_game
+            })
     
     return game_list
